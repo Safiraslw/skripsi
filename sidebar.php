e40e66941c0619e2264259a4ea474bfe9a6e08ed
Safiraslw <90440818+Safiraslw@users.noreply.github.com>
2024-05-25 15:16:26 +0700

    first commit

---
+++ sidebar.php
@@ -0,0 +1,69 @@
+<!-- Sidbar -->
+<div class="navigation">
+    <h1><img src="image/udang.jpg" alt=""></h1>
+    <ul>
+        <li id="home">
+            <a href="dashboard.php" class="nav-link">
+                <i class="icon"><ion-icon name="home-outline"></ion-icon></i>
+                <span>Home</span>
+            </a>
+        </li>
+        <li id="peta">
+            <a href="data_training.php" class="nav-link">
+                <i class="icon"><ion-icon name="location-outline"></ion-icon></i>
+                <span>Data Training</span>
+            </a>
+        </li>
+        <li id="peta">
+            <a href="data_uji.php" class="nav-link">
+                <i class="icon"><ion-icon name="location-outline"></ion-icon></i>
+                <span>Data Uji</span>
+            </a>
+        </li>
+        <li id="ecom">
+            <a href="#ecom" class="nav-link">
+                <i class="icon"><ion-icon name="folder-outline"></ion-icon></i>
+                <span>Prediksi</span>
+                <i class="fa fa-angle-right"></i>
+            </a>
+            <ul>
+                <li>
+                    <a href="prediksi_udang.php" class="dropdown" style="height: 15px; font-size: small;">Prediksi Udang</a>
+                </li>
+                <li>
+                    <a href="prediksi_bileh.php" class="dropdown" style="height: 15px; font-size: small;">Prediksi Bileh</a>
+                </li>
+            </ul>
+        </li>
+        <li id="rawat">
+            <a href="#" class="nav-link">
+                <i class="icon"><ion-icon name="documents-outline"></ion-icon></i>
+                <span>Grafik</span>
+            </a>
+        </li>
+        <li id="ecom1">
+            <a href="#ecom1" class="nav-link">
+                <i class="icon"><ion-icon name="folder-outline"></ion-icon></i>
+                <span>Laporan</span>
+                <i class="fa fa-angle-right"></i>
+            </a>
+            <ul>
+                <li>
+                    <a href="laporan_udang.php" class="dropdown">Laporan Produksi Udang</a>
+                </li>
+                <li>
+                    <a href="laporan_bileh.php" class="dropdown">Laporan Produksi Bileh</a>
+                </li>
+            </ul>
+        </li>
+        <li id="rawat">
+            <a href="proses/logout.php" class="nav-link" style="margin-top: 5px;">
+                <span>Logout</span>
+            </a>
+        </li>
+    </ul>
+</div>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+<!-- Akhir Sidebar -->
